b13a8907557cb44115463a7b711bc94de7474ae6
MSG-G <mouhagueye97@gmail.com>
2026-02-14 00:50:21 +0000

    Deploy : github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/projet-saint-valentin/' : '/',
   server: {
     host: "::",
     port: 8080,
